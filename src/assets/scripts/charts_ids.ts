47f983b897c5e19b830498e6e3f78cd732c73d17
merkaz-dev <merkaz.developer@gmail.com>
2021-01-12 04:29:13 +0300

    add three questions

--- src/assets/scripts/charts_ids.ts
+++ src/assets/scripts/charts_ids.ts
@@ -1,12 +1,108 @@
 import { ChartsIds } from 'src/models/question-display';
 
 export function getChartsIds(): ChartsIds[] {
-  const q = {
+  const q1 = {
     question_id: 1,
     chartsIds: [
       '10578d98-04ac-4965-b27d-33868b9c36d3',
       'bac00933-1a23-4ff4-a7c7-4f59cd7b24e9',
     ],
   };
-  return [q];
+  const q2 = {
+    question_id: 2,
+    chartsIds: [
+      '3b32f86a-55ad-4d39-913f-363ac4337755',
+      '69124452-b2ec-49e2-979f-13b4ecb3543a',
+    ],
+  };
+  const q3 = {
+    question_id: 3,
+    chartsIds: [
+      '8b4d33b8-0634-4e6e-81e0-304b8252e6ed',
+      'dd656359-7c81-449d-99c2-184754010fe7',
+      '83f5537f-377b-4aba-b9c6-f0dc6dbd1d7b',
+    ],
+  };
+  const q4 = {
+    question_id: 4,
+    chartsIds: ['', '', ''],
+  };
+  const q5 = {
+    question_id: 5,
+    chartsIds: ['', '', ''],
+  };
+  const q6 = {
+    question_id: 6,
+    chartsIds: ['', '', ''],
+  };
+  const q7 = {
+    question_id: 7,
+    chartsIds: ['', '', ''],
+  };
+  const q8 = {
+    question_id: 8,
+    chartsIds: ['', '', ''],
+  };
+  const q9 = {
+    question_id: 9,
+    chartsIds: ['', '', ''],
+  };
+  const q10 = {
+    question_id: 10,
+    chartsIds: ['', '', ''],
+  };
+  const q11 = {
+    question_id: 11,
+    chartsIds: ['', '', ''],
+  };
+  const q12 = {
+    question_id: 12,
+    chartsIds: ['', '', ''],
+  };
+
+  const q13 = {
+    question_id: 13,
+    chartsIds: ['', '', ''],
+  };
+  const q14 = {
+    question_id: 14,
+    chartsIds: ['', '', ''],
+  };
+  const q15 = {
+    question_id: 15,
+    chartsIds: ['', '', ''],
+  };
+  const q16 = {
+    question_id: 16,
+    chartsIds: ['', '', ''],
+  };
+  const q17 = {
+    question_id: 17,
+    chartsIds: ['', '', ''],
+  };
+  const q18 = {
+    question_id: 18,
+    chartsIds: ['', '', ''],
+  };
+
+  return [
+    q1,
+    q2,
+    q3,
+    q4,
+    q5,
+    q6,
+    q7,
+    q8,
+    q9,
+    q10,
+    q11,
+    q12,
+    q13,
+    q14,
+    q15,
+    q16,
+    q17,
+    q18,
+  ];
 }
--- test.js
+++ test.js
@@ -1,11 +1,130 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// Define by age
+db.answers.aggregate([
+  {
+    $project: {
+      _id: 0,
+      city: "$user_info.city",
+      age: { $arrayElemAt: ["$questions", 1] },
+    },
+  },
 
-console.log(getRandomInt(0, 2));
-console.log(getRandomInt(0, 2));
-console.log(getRandomInt(0, 2));
-console.log(getRandomInt(0, 2));
-console.log(getRandomInt(0, 2));
+  { $unwind: "$age.question_answers" },
+  {
+    $group: {
+      _id: {
+        range: "$age.question_answers.answer_text",
+        yes: "$age.question_answers.answer_boolean_reply",
+      },
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project: { _id: 0, range: "$_id.range", yes: "$_id.yes", count: "$count" },
+  },
+  { $match: { yes: true } },
+  { $project: { "Возрастная группа": "$range", Количество: "$count" } },
+]);
+
+// СООТНОШЕНИЕ ПО ПОЛУ И ВОЗРАСТУ
+db.answers.aggregate([
+  {
+    $project: {
+      _id: 0,
+      ip: "$user_info.ip",
+      age: { $arrayElemAt: ["$questions", 1] },
+      sex: { $arrayElemAt: ["$questions", 0] },
+    },
+  },
+
+  { $unwind: "$age.question_answers" },
+  { $unwind: "$sex.question_answers" },
+  {
+    $group: {
+      _id: {
+        person: "$ip",
+        age: "$age.question_answers.answer_text",
+        age_exists: "$age.question_answers.answer_boolean_reply",
+        sex: "$sex.question_answers.answer_text",
+        sex_exists: "$sex.question_answers.answer_boolean_reply",
+      },
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      age: "$_id.age",
+      age_exists: "$_id.age_exists",
+      sex: "$_id.sex",
+      sex_exists: "$_id",
+    },
+  },
+  { $match: { $and: [{ age_exists: true }, { sex_exists: true }] } },
+  { $project: { "Возрастная группа": "$age", Пол: "$sex" } },
+]);
+
+// СООТНОШЕНИЕ ПО ПОЛУ И ВОЗРАСТУ И СТАЖУ ТРЕНИРОВОК
+db.answers.aggregate([
+  {
+    $project: {
+      _id: 0,
+      ip: "$user_info.ip",
+      sex: { $arrayElemAt: ["$questions", 0] },
+      age: { $arrayElemAt: ["$questions", 1] },
+      training: { $arrayElemAt: ["$questions", 2] },
+    },
+  },
+
+  { $unwind: "$sex.question_answers" },
+  { $unwind: "$age.question_answers" },
+  { $unwind: "$training.question_answers" },
+  {
+    $group: {
+      _id: {
+        person: "$ip",
+        sex: "$sex.question_answers.answer_text",
+        sex_exists: "$sex.question_answers.answer_boolean_reply",
+        age: "$age.question_answers.answer_text",
+        age_exists: "$age.question_answers.answer_boolean_reply",
+        training: "$training.question_answers.answer_text",
+        training_exists: "$training.question_answers.answer_boolean_reply",
+      },
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      age: "$_id.age",
+      age_exists: "$_id.age_exists",
+      sex: "$_id.sex",
+      sex_exists: "$_id.sex_exists",
+      training: "$_id.training",
+      training_exists: "$_id.training_exists",
+    },
+  },
+  {
+    $match: {
+      $and: [
+        { age_exists: true },
+        { sex_exists: true },
+        { training_exists: true },
+      ],
+    },
+  },
+  {
+    $group: {
+      _id: { sex: "$sex", age: "$age", training: "$training" },
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      Пол: "$_id.sex",
+      Возраст: "$_id.age",
+      "Стаж занятий ОФП": "$_id.training",
+      Количество: "$count",
+    },
+  },
+]);
